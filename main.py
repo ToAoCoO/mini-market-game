00320955a5f4185892aa8466b2033a2cdcd80c5c
TACO <ch85221@gmail.com>
2026-03-08 21:30:33 +0800

    用函數改變狀態

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
 import random
-game_data = {
-"current_round": 0,
-"money": 0 ,
-"item": ["A", "B"] ,
-"backpack": [0, 0],
-"prices": [0,0]
-}
 
 
 def init():
-    game_data["current_round"] = 1
-    game_data["money"] = 1000
-    game_data["backpack"] = [0, 0]
-    game_data["prices"] = [0, 0]
-
+    game_data = {
+    "current_round": 1,
+    "money": 1000 ,
+    "item": ["A", "B"] ,
+    "backpack": [0, 0],
+    "prices": [0,0]
+    }
     print("遊戲已重置！")
+    return game_data
 
-def buy(target: str, num: int)  -> str:
+def buy(target: str, num: int, game_data: dict)  -> str:
     cost = num * game_data["prices"][game_data["item"].index(target)]
     if cost <= game_data["money"]:
         game_data["money"] -= cost
@@ -25,17 +21,17 @@ def buy(target: str, num: int)  -> str:
     else:
         return "餘額不足！"
 
-def sell(target: str, num: int)  -> str:
+def sell(target: str, num: int, game_data: dict)  -> str:
     cost = num * game_data["prices"][game_data["item"].index(target)]
     if num <= game_data["backpack"][game_data["item"].index(target)]:
-        game_data["money"][0] += cost
+        game_data["money"] += cost
         game_data["backpack"][game_data["item"].index(target)] -= num
         return f"成功賣出 {num} 個 {target}"          
     else:
         return "存貨不足！"
 
 
-def game_loop():
+def game_loop(game_data: dict) -> str:
     while game_data["current_round"] <= 5:
         game_data["prices"] = [ random.randint(100, 200), random.randint(100, 200) ]
         
@@ -52,16 +48,16 @@ def game_loop():
                     print("無效貨物名稱！")
                     continue
                 num = int(input(f"想要買多少個 {target}？: "))
-                print(buy(target, num))
+                print(buy(target, num, game_data))
             elif action == "2":
                 target = input("請輸入貨物名稱 (A/B): ").upper()
                 if target not in game_data['item']:
                     print("無效貨物名稱！")
                     continue
                 num = int(input(f"想要賣多少個 {target}？: "))
-                print(sell(target, num))
+                print(sell(target, num, game_data))
             elif action == "3":
-                game_data["current_round"][0] += 1
+                game_data["current_round"] += 1
                 break
             else:
                 print("無效指令")
@@ -69,8 +65,8 @@ def game_loop():
 
         
     
-    return"\n遊戲結束！" f"最終資產: {game_data['money'][0]}"   
+    return"\n遊戲結束！" f"最終資產: {game_data['money']}"   
 
 if __name__ == "__main__":
-    init()
-    game_loop()
+    game_data_now = init()
+    game_loop(game_data_now)
